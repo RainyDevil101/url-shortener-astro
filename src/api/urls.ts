0315eb37745ef46be4d8f7fccb2f66cbe838850d
Alexis Herrera <desert5@outlook.es>
2024-02-15 18:02:14 -0300

    baseurlupdated

--- src/api/urls.ts
+++ src/api/urls.ts
@@ -1,3 +1,3 @@
-export const baseUrl = "http://localhost:4321/";
+export const baseUrl = "https://shrt-url-ah.netlify.app/";
 export const backendUrl = "https://url-shortener-dlcc.onrender.com/api/v2/url";
 // export const backendUrl = "http://localhost:3001/api/v2/url";
