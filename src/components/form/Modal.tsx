19997e3d5a5512a9fb585b7a391f343f57624aed
Alexis <desert5@outlook.es>
2025-07-22 20:13:13 -0400

    Refactor: Standardize quotation marks and improve code formatting across multiple components

--- src/components/form/Modal.tsx
+++ src/components/form/Modal.tsx
@@ -1,28 +1,29 @@
-import type React from "preact/compat";
+import type React from 'preact/compat';
 
 interface ModalProps {
   shortUrl: string;
 }
 
-export const Modal: React.FC<ModalProps> = ({shortUrl}) => {
-
-
-
-
-    return (
-        <>
-            <dialog id="id_shorted" className="modal">
-                <div className="modal-box">
-                    <h3 className="font-bold text-2xl mb-4">Link acortado 👍!</h3>
-                    <input type="text" value={shortUrl} placeholder="Type here" className="input input-bordered input-accent w-full" />
-                    <div className="modal-action mt-8">
-                        <form method="dialog">
-                            {/* if there is a button in form, it will close the modal */}
-                            <button className="btn">Aceptar</button>
-                        </form>
-                    </div>
-                </div>
-            </dialog>
-        </>
-    );
+export const Modal: React.FC<ModalProps> = ({ shortUrl }) => {
+  return (
+    <>
+      <dialog id="id_shorted" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-2xl mb-4">Link acortado 👍!</h3>
+          <input
+            type="text"
+            value={shortUrl}
+            placeholder="Type here"
+            className="input input-bordered input-accent w-full"
+          />
+          <div className="modal-action mt-8">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Aceptar</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+    </>
+  );
 };
